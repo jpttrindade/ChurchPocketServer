57dc3a8e9aa0d299fdd4bd6beb12d39fedc44730
Joao Paulo <tenoriojpaulo@gmail.com>
2016-11-18 02:40:01 -0300

    bugifx response pastorais (response code)

--- routes/pastorais.js
+++ routes/pastorais.js
@@ -12,7 +12,7 @@ router.get('/', function (req, res) {
             res.sendStatus(500);
         } else {
             console.log(pastorais);
-            res.setStatus(200);
+            res.statusCode = (200);
             res.json({pastorais:pastorais});
         }
     });
